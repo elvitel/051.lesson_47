b1df67000a0c860c2b168e2adbd45e59e1f1715a
Elena Tykhonova <sivak_elena@ukr.net>
2023-08-07 00:31:14 +0300

    home47 is added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import data from './data.json';
+import ToDoList from './components/ToDoList';
+import ToDoForm from './components/ToDoForm';
 import './App.css';
 
 function App() {
+  const [ toDoList, setToDoList ] = useState(data);
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
+
+  const addTask = (userInput) => {
+    let copy = [...toDoList];
+    copy.push({ id: toDoList.length + 1, task: userInput, complete: false });
+    setToDoList(copy);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Todo-List</h2>
+      <ToDoList toDoList = { toDoList } handleToggle={handleToggle} />
+      <ToDoForm addTask={addTask} />
     </div>
   );
 }
 
 export default App;
+
